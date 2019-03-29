f21dc3cc506c09f449d1514fe3fd5bb2d76408fb
Lucas <lucas.taddei@unil.ch>
2019-03-29 21:14:10 +0100

    acadcadcadsca

--- UF/imports/ui/scripts/selectionFlash.js
+++ UF/imports/ui/scripts/selectionFlash.js
@@ -6,14 +6,16 @@ import '../templates/selectionFlash.html'
 if (Meteor.isClient) {
 
     Template.types.events({
-       'click #games': function() {
-          alert("GAMEEEE ONNN");
-       },
-       'click #drink': function(){
-           alert("You will never drink alone!");
-       },
-       'click #music': function(){
-           alert("DO RE MI FA SOL LA SOOOOOOL")
-       }
-    });
- }
+       //'click #games': function() {
+       //   alert("GAMEEEE ONNN");
+       //},
+       //'click #drink': function(){
+       //    alert("You will never drink alone!");
+       //},
+       //'click #music': function(){
+       //      alert("DO RE MI FA SOL LA SOOOOOOL")
+       //},
+       //"click #games": function(){
+       ////    if($("#games").css({"background-color":"#FFD500"})){
+       //     $("#games").css({"background-color":"lightgreen"})
+       //
