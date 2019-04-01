645305ce0db0de49e0cbb4f4458b53a3fca2b833
Jason <olajason@yahoo.fr>
2019-04-01 13:26:59 +0200

    Update selectionFlash.js
    J ai modifié comment t as fait les commentaires ca buggait

--- UF/imports/ui/scripts/selectionFlash.js
+++ UF/imports/ui/scripts/selectionFlash.js
@@ -3,19 +3,19 @@ import { Template } from 'meteor/templating';
 
 import '../templates/selectionFlash.html'
 
-if (Meteor.isClient) {
+/*if (Meteor.isClient) {
 
     Template.types.events({
-       //'click #games': function() {
-       //   alert("GAMEEEE ONNN");
-       //},
-       //'click #drink': function(){
-       //    alert("You will never drink alone!");
-       //},
-       //'click #music': function(){
-       //      alert("DO RE MI FA SOL LA SOOOOOOL")
-       //},
-       //"click #games": function(){
-       ////    if($("#games").css({"background-color":"#FFD500"})){
-       //     $("#games").css({"background-color":"lightgreen"})
-       //
+       'click #games': function() {
+         alert("GAMEEEE ONNN");
+       },
+       'click #drink': function(){
+           alert("You will never drink alone!");
+       },
+       'click #music': function(){
+             alert("DO RE MI FA SOL LA SOOOOOOL")
+       },
+       "click #games": function(){
+       if($("#games").css({"background-color":"#FFD500"})){
+       $("#games").css({"background-color":"lightgreen"})*/
+
