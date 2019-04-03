e04b75d47bce932eba892c9e699f8620853ee88c
Lucas <lucas.taddei@unil.ch>
2019-04-03 09:59:02 +0200

    asodkcmad

--- UF/server/main.js
+++ UF/server/main.js
@@ -4,3 +4,7 @@ Meteor.startup(() => {
   // code to run on server at startup
 });
 
+//userInfos = new Mongo.collection("User")
+//var userEmail = document.getElementbyId("login-email");
+//var userPassword = document.getElementbyID("login-password");
+//userInfos.insert({email: userEmail, passowrd: userPassword})
