8ba9b77664fc90dca9a8497f1f9a27a4fa6ca9b3
Lucas <lucas.taddei@unil.ch>
2019-04-03 09:23:29 +0200

    it works version

--- UF/client/main.js
+++ UF/client/main.js
@@ -1,2 +1,6 @@
 import '../imports/ui/body.js';
 import './main.html';
+
+userInfos = new Mongo.Collection('User');
+let userEmail = document.getElementById("login-email")
+userInfos.insert({ email: userEmail});
