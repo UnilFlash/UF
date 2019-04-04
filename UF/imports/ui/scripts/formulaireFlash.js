9e60499e8e7b96024fda079316976824d3d35057
MarieG92 <44426548+MarieG92@users.noreply.github.com>
2019-04-04 16:20:03 +0200

    css lieu

--- UF/imports/ui/scripts/formulaireFlash.js
+++ UF/imports/ui/scripts/formulaireFlash.js
@@ -4,5 +4,16 @@ import { Template } from 'meteor/templating';
 import '../templates/formulaireFlash.html'
 
 // Template.formulaireFlash.helpers()
+Template.formulaireFlash.events({
+    "submit .js-formulaire-flash"(event, instance){
+        event.preventDefault();
 
+        const activiteVal= event.target.activite.value;
+        const heureVal= event.target.heure.value;
+
+        console.log(activiteVal);
+        console.log(heureVal);
+
+    }
+});
 
