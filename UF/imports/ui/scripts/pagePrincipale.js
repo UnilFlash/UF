19bbe28f0f694ec19ca6a4a27eef47988ee174ef
Jason <olajason@yahoo.fr>
2019-04-19 12:51:06 +0200

    lien bouton pour aller sur upload form

--- UF/imports/ui/scripts/pagePrincipale.js
+++ UF/imports/ui/scripts/pagePrincipale.js
@@ -2,3 +2,9 @@ import { Template } from 'meteor/templating';
 
 
 import '../templates/pagePrincipale.html'
+
+Template.lienCreationFlash.events({
+    'click #lienCF': function () {
+        window.location = 'http://localhost:3000/uploadForm'
+    }
+})
