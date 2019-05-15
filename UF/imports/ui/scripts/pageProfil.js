0f19eb544577a9b9af001a345493ba2738906664
Lara <laridelidelo@gmail.com>
2019-05-15 10:28:17 +0200

    profile

--- UF/imports/ui/scripts/pageProfil.js
+++ UF/imports/ui/scripts/pageProfil.js
@@ -2,3 +2,10 @@ import { Template } from 'meteor/templating';
 
 
 import '../templates/pageProfil.html'
+
+
+Template.deconnexion.events({
+	'click .js-logout'(event, instance){
+		FlowRouter.go('/');
+	}
+});
