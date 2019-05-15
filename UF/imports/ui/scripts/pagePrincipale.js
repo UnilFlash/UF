f86e9e5e7edbb93d914ea4c16de337eeb84f7bb9
Jason <olajason@yahoo.fr>
2019-05-15 11:05:54 +0200

    LIAISON BASE DE DONNEES IMAGE ET FLASHS

--- UF/imports/ui/scripts/pagePrincipale.js
+++ UF/imports/ui/scripts/pagePrincipale.js
@@ -1,6 +1,7 @@
 import { Template } from 'meteor/templating';
 //import { Flash } from '../../api/flash';
 import { Images } from '../../api/img.js';
+import { Flash } from '../../api/flash.js';
 
 import '../templates/pagePrincipale.html';
 
@@ -16,8 +17,14 @@ Template.lienCreationFlash.helpers({
     currentUpload() {
       return Template.instance().currentUpload.get();
     },
-    imageFile(){
-        return Images.find()
+    flash(){
+        return Flash.find()
+    },
+    image(){
+        let flashId = this._id;
+        let image = Flash.findOne({_id: flashId}, {fields: {idImage: 1}})
+        let returnedImage = Images.findOne({_id: image.idImage})
+        return returnedImage
     }
   });
   
