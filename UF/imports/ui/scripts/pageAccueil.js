200c361a239c3b8b79f1e543e15175416902e478
Lara <laridelidelo@gmail.com>
2019-05-15 08:55:39 +0200

    Correction d'erreurs

--- UF/imports/ui/scripts/pageAccueil.js
+++ UF/imports/ui/scripts/pageAccueil.js
@@ -1,60 +1,42 @@
-import { Template } from 'meteor/templating';
-
-import '../templates/pageAccueil.html'
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+import { Template }   from 'meteor/templating';
+import '../templates/pageAccueil.html'
 
 
 Template.navbar.events({
-    'click .js-open-login-modal'(event, instance){
-
-        Modal.show('login_modal');
-
-    },
-    
-    'click .js-logout'(event, instance){
-
-        Meteor.logout();
-    },
-
+	'click .js-open-login-modal'(event, instance){
+		Modal.show('login_modal');
+	}
 });
 
 
 Template.login_modal.onCreated(function(){
-    this.autorun(()=>{
-        if(Meteor.userId()){
-            Modal.hide('login_modal');
-        }
-    });
-
+	this.autorun(()=>{
+		if(Meteor.userId()){
+			const titre = document.querySelector("h3").innerHTML;
+			if(titre == "Se connecter") FlowRouter.go("/main");
+			else FlowRouter.go("/filtres");
+		}
+	});
 });
+
 
 T9n.setLanguage('fr');
-
-let email = AccountsTemplates.removeField('email');
-let password = AccountsTemplates.removeField('password');
-
+const email = AccountsTemplates.removeField('email');
+const password = AccountsTemplates.removeField('password');
 AccountsTemplates.addField({
-    _id: 'fullname',
-    type: 'text',
-    displayName: 'Nom complet',
-    placeholder: 'Nom complet',
-    required: true,
-    minLength: 3,
-    trim: true,
-    re:/(?=.*[a-z])(?=.*[A-Z])/,
-    errStr:'Au moins 1 minuscule et 1 majuscule',
+	_id: 'fullname',
+	type: 'text',
+	displayName: 'Nom complet',
+	placeholder: 'Nom complet',
+	required: true,
+	minLength: 3,
+	trim: true,
+	re:/(?=.*[a-z])(?=.*[A-Z])/,
+	errStr:'Au moins 1 minuscule et 1 majuscule',
 });
-
 password.re=/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/,
 password.errStr= 'Au moins 1 chiffre, 1 minuscule et 1 majuscule',
 password.minLength = 6;
-
-
 AccountsTemplates.addField(email);
 AccountsTemplates.addField(password);
-
-
-
-
-    
-
